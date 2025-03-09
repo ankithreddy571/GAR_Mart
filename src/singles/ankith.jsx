0029118f907aa01f6967e2f10a1cdd2afb835466
akhilreddy13UCM <axe55580@ucmo.edu>
2025-03-09 12:20:54 -0500

    import

--- src/singles/ankith.jsx
+++ src/singles/ankith.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { acData } from '../stores/data/ac'
+ { acData } from '../stores/data/ac'
 import { useParams } from 'react-router-dom'
 import Navbar from '../stores/components/Navbar'
 
