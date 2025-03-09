79e3e6a0e5b94e6cbd5a7fa19df369d710b17a07
ankithreddy571 <70102630+ankithreddy571@users.noreply.github.com>
2025-03-08 21:27:47 -0500

    Added ankith.jsx

--- src/singles/ankith.jsx
+++ src/singles/ankith.jsx
@@ -12,7 +12,7 @@ const ACSingle = () => {
 
     const product = acData.find((item)=>item.id===id);
     if (!product) {
-        return <div>Product not found</div>;
+        return <div>Product found</div>;
     }
 
 
